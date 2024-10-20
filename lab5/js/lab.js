be3bc8c68f00cefbe4f9b914fe6f78050f7e1370
FantomFrenzy <ronieantonioarts@gmail.com>
2024-10-20 00:54:43 -0700

    Creating a JavaScript

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,16 +1,26 @@
 // index.js - purpose and description here
-// Author: Your Name
-// Date:
+/* 
+ *  Author: Ronie Antonio and Ben Awtry
+ *  Emails: <rantoni1@ucsc.edu> and <bawtry@ucsc.edu>
+ *  Created: 20 Oct 2024
+ *  License: Public Domain
+ */
 
-// Constants
+// Defining Variables
+make = "Toyota";
+model = "Camry";
+color = "Blue";
+year = 2010;
 
-// Functions
+// Calculating Age of Car
+age = 2024 - year;
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
+// Will it output?
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
 
 function main() {
   console.log("Main function started.");
