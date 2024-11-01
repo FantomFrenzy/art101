72801f2759af1f481b48b1093fcd9a75bf1e7006
FantomFrenzy <ronieantonioarts@gmail.com>
2024-10-31 19:55:15 -0700

    Lab 8 finalization

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -1,21 +1,33 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+/* 
+ *  Author: Ronie Antonio and Ben Awtry
+ *  Emails: <rantoni1@ucsc.edu> and <bawtry@ucsc.edu>
+ *  Created: 28 Oct 2024
+ *  License: Public Domain
+ */
 
-// Constants
+// Create an array
+var numbers = [4, 7, 5, 9];
+console.log("Our array: ", numbers);
 
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+// Function made to add 4
+function addFour(x) {
+    return x + 4;
 }
+//Test the function
+console.log("3 + 4 = ", addFour(3));
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
+// Add four to the array
+var numbersAddFour = numbers.map(addFour);
+console.log("Updated array (+4): ", numbersAddFour);
 
-// let's get this party started
-main();
+// Multiple the array by three
+var numbersTimesThree = numbers.map(function(x) {
+  return x * 3;
+})
+// Test the anon function
+console.log("Updated array (3x): ", numbersTimesThree);
+
+// Output
+$("#output").html("Original Array: " + numbers + "</br>" + 
+  "Updated Array (+4): " + numbersAddFour + "</br>" + 
+  "Udpated Array (3x): " + numbersTimesThree);
