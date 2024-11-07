704f34e02f5454c8da4bc08861c6e0aff4f4814f
FantomFrenzy <ronieantonioarts@gmail.com>
2024-11-07 13:25:47 -0800

    major

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -5,11 +5,10 @@
  *  License: Public Domain
  */
 
-// Helper function generates fake dialogue
 function generateRandomText() {
   const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
   const min = 3;
-  const max = 200;
+  const max = 100;
   const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
   // Get a random starting index to slice the Lorem Ipsum text
   const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
@@ -17,11 +16,20 @@ function generateRandomText() {
   return text.slice(randStart, randStart + randLen);
 }
 
-// click listener for button
+// Click listener for button
 $("#make-convo").click(function(){
-  // get new fake dialogue
+  // Get new fake dialogue
   const newText = generateRandomText();
-
-  // append a new div to our output div
+  
+  // Append a new div to our output div
   $("#output").append('<div class="text"><p>' + newText + '</p></div>');
+});
+
+// Click listener for Submit button
+$("#submitButton").click(function(){
+  // Get userInput value
+  var input = $("#userInput").val();
+
+  // Append a new div to our output div
+  $("#output").append('<div class="userText"><p>' + input + '</p></div>');
 });
