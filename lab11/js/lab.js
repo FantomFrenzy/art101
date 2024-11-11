ec9e2d381ebddc09f3f6f79b142810d22c6641be
FantomFrenzy <ronieantonioarts@gmail.com>
2024-11-10 23:54:52 -0800

    Lab 11 Final changes

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -1,21 +1,30 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+/* 
+ *  Author: Ronie Antonio and Ben Awtry
+ *  Emails: <rantoni1@ucsc.edu> and <bawtry@ucsc.edu>
+ *  Created: 10 November 2024
+ *  License: Public Domain
+ */
 
-// Constants
-
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+// Sorts the characters of a string in alphabetical order.
+function sortString(inputString) {
+    // Convert our string to an array and to sort it back again
+    const sortedString = inputString.replace(/\s+/g, '').toLowerCase().split('').sort().join('');
+    // additional functions added -  .replace() used to remove spaces
+    // additional functions added - .toLowerCase() makes upper cases to lower cases
+    return sortedString.charAt(0).toUpperCase() + sortedString.slice(1);
+    // capitalizes first character and combine it with the rest of the sorted string
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
+// Click listener for button
+$("#submit").click(function() {
+    // Get value of input field
+    const userName = $("#user-name").val();
 
-// let's get this party started
-main();
+    // Sort the user's name
+    const userNameSorted = sortString(userName);
+
+    // Append a new div to our output div with the sorted name
+    $("#output").html('<div class="text"><p>' + userNameSorted + '</p></div>');
+    $("#secret").html('<div class="text"><p>HA! You\'ve been cursed. You cannot change back.</p></div>');
+});
+
