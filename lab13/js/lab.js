e6d4b266cc996f50cf811fb76f4f20285e6e7692
FantomFrenzy <ronieantonioarts@gmail.com>
2024-11-17 17:31:11 -0800

    Lab 13 Loops

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -1,21 +1,40 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+/* 
+ *  Author: Ronie Antonio and Ben Awtry
+ *  Emails: <rantoni1@ucsc.edu> and <bawtry@ucsc.edu>
+ *  Created: 12 November 2024
+ *  License: Public Domain
+ */
 
-// Constants
+function failureManagement() {
+    // Variable to hold output string
+    let longString = "";
+  
+    // Loop through numbers 1 to 250
+    for (let num = 0; num <= 250; num++) {
+        // Initialize an empty string for each number's output
+        let output = "";
 
-// Functions
+        // Check divisibility and append corresponding words
+        if (num % 3 == 0) {
+            output += "Welcome to ";
+        } if (num % 5 == 0) {
+            output += "Failure ";
+        } if (num % 7 == 0) {
+            output += "Management"
+        } if (num % 3 == 0 && num % 5 == 0 && num % 7 == 0) {
+            output += "!"; //just the added exclamation mark :)
+        } if (output == "") {
+            output += "";
+        } // If output string is still empty, 
+          // the number is not a multiple of 3, 5, or 7
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
+        // Append the number and the output to longString; revised by ChatGPT
+        longString += `<div>${num}: ${output}</div>`;
+    }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+    // Output the final result to the #output div
+    document.getElementById("output").innerHTML = longString;
+  }
+  
+  // Call function
+  failureManagement();
