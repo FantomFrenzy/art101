2cd466bb4d255a8a8692227a2a83b95e35a6f0eb
FantomFrenzy <ronieantonioarts@gmail.com>
2024-12-02 15:30:37 -0800

    Update lab.js

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -5,39 +5,114 @@
  *  License: Public Domain
  */
 
-// Bind a click event to the button with ID "activate"
-$("#activate").click(function() {
-        // Fetch the XKCD comic data using AJAX
-        $.ajax({
-            // URL for the request to fetch comic data
-            url: "https://xkcd.com/info.0.json",
-            // Type of request (GET)
-            type: "GET",
-            // Expecting JSON data from the API
-            dataType: "json",
-            // Success callback function when the request succeeds
-            success: function(comicObj) {
-                // Clear the previous content in the output div
-                $("#output").empty();
+// // Bind a click event to the button with ID "activate"
+// $("#activate").click(function() {
+//         // Fetch the XKCD comic data using AJAX
+//         $.ajax({
+//             // URL for the request to fetch comic data
+//             url: "https://xkcd.com/info.0.json",
+//             // Type of request (GET)
+//             type: "GET",
+//             // Expecting JSON data from the API
+//             dataType: "json",
+//             // Success callback function when the request succeeds
+//             success: function(comicObj) {
+//                 // Clear the previous content in the output div
+//                 $("#output").empty();
 
-                // Create the title element with the comic title
-                var comicTitle = $("<h3></h3>").text(comicObj.title);
+//                 // Create the title element with the comic title
+//                 var comicTitle = $("<h3></h3>").text(comicObj.title);
                 
-                // Create the image element with the comic image
-                var comicImage = $("<img />", {
-                    src: comicObj.img,       // Set image source from the object
-                    alt: comicObj.alt,       // Set alt text from the object
-                    title: comicObj.alt      // Set title text to match the alt attribute
-                });
+//                 // Create the image element with the comic image
+//                 var comicImage = $("<img />", {
+//                     src: comicObj.img,       // Set image source from the object
+//                     alt: comicObj.alt,       // Set alt text from the object
+//                     title: comicObj.alt      // Set title text to match the alt attribute
+//                 });
                 
-                // Append the title and image to the output div
-                $("#output").append(comicTitle).append(comicImage);
-            },
-            // Error callback if the request fails
-            error: function(jqXHR, textStatus, errorThrown) {
-                // Log any errors to the console and display an error message in the output div
-                console.log("Error:", textStatus, errorThrown);
-                $("#output").text("Failed to load the comic. Please try again later.");
-            }
-        });
-    });
+//                 // Append the title and image to the output div
+//                 $("#output").append(comicTitle).append(comicImage);
+//             },
+//             // Error callback if the request fails
+//             error: function(jqXHR, textStatus, errorThrown) {
+//                 // Log any errors to the console and display an error message in the output div
+//                 console.log("Error:", textStatus, errorThrown);
+//                 $("#output").text("Failed to load the comic. Please try again later.");
+//             }
+//         });
+//     });
+
+var URL = "https://xkcd.com/info.0.json";
+
+// https://xkcd.com/json.html
+// endpoint https://xkcd.com/456/info.0.json
+var URLpre = "https://xkcd.com/";
+var URLpost = "info.0.json";
+
+// Endpoint format: http://xkcd.com/614/info.0.json
+
+// a helper function to turn single and double quotes into
+// html symbols so they don't confuse html tags
+//
+function make_safe(str) {
+  return str.replace(/'/g, '&apos;').replace(/"/g, '&quot;');
+}
+
+function getComic(num) {
+  if (typeof num === 'undefined') {
+    numStr = "";
+  } else {
+    numStr = num.toString() + "/";
+  }
+  var ourURL = URLpre + numStr + URLpost;
+  console.log("Our new URL:", ourURL);
+  // get data via ajax from numbersapi
+  // Using the core $.ajax() method
+  $.ajax({
+      // The URL for the request (ENDPOINT)
+      url: ourURL,
+      // Whether this is a POST or GET request
+      type: "GET",
+  })
+  // If the request succeeds
+  .done(function(data) {
+      // console.log(data);
+      var imageUrl = data.img;
+      // we use .replace(/"/g, '\\"') after the text strings because
+      // sometimes there are single and double quotes in the text
+      // that confuses the html
+      var title = data.title;
+      console.log("orig title:", title);
+      title = make_safe(title);
+      console.log("safe title:", title);
+      var alt = data.alt;
+      console.log("orig alt:", alt);
+      alt = make_safe(alt);
+      console.log("safe alt:", alt);
+      var comicNum = data.num;
+      var html = `<div id="imageblock">
+          <h2>${title}</h2>
+          <img src="${imageUrl}" title="${alt}"><br>
+          <button id="prev">Previous</button><button id="next">Next</button>
+        </div>`
+      // console.log("My new html: \n", html);
+      $("#output").html(html);
+
+      // add event listener to new prev button
+      $("#prev").click(function(){
+        getComic(comicNum - 1);
+      });
+      // add event listener to new next button
+      $("#next").click(function(){
+        getComic(comicNum + 1);
+      });
+  })
+  .fail(function(){
+    console.log("^^ Please ignore this error. It's okay.");
+    console.log("Have a okay day! :-)");
+  })
+
+}
+
+// start things off
+getComic();
